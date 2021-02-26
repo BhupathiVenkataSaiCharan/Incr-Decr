4157fbca28cff1f7d37b3419a1546bb4c48872dd
Cherry <77479689+BhupathiVenkataSaiCharan@users.noreply.github.com>
2021-02-26 18:09:48 +0530

    Create ToDoList.js

---
+++ src/ToDoList.js
@@ -0,0 +1,35 @@
+import React,{useState} from "react";
+
+const ToDoList = () => {
+
+    const [num, setNum] = useState(0);
+
+    const incNum = () =>{
+        setNum(num+1);
+    };
+
+    const decNum = () =>{
+        if(num>0){
+            setNum(num-1);
+        }else{
+            alert("Sorry, Zero Limit Reached");
+            setNum(0);
+        }
+        
+    };
+
+    return (
+    <>
+    <div className="main_div">
+        <div className="center">
+            <h1> {num} </h1>
+            <div className="btn_div">
+                <button onClick={incNum}> Increm </button>
+                <button onClick={decNum}> Decrem </button>
+            </div>
+        </div>
+    </div>
+    </>);
+};
+
+export default ToDoList;
